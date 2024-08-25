25db7dd9f0d3c7b276e3e70fc246d37ecc86d3c4
dradm1r <>
2024-08-25 21:21:42 +0300

    [enhancment] +gitignore

---
+++ Figures.Core/Math/Interfaces/MeasureWrapper.cs
@@ -0,0 +1,31 @@
+﻿namespace Figures.Core.Math.Interfaces;
+
+public abstract class MeasureWrapper<TMeasure> :
+    IEquatable<MeasureWrapper<TMeasure>>,
+    IComparable
+{
+    protected MeasureWrapper(TMeasure value)
+    {
+        Value = value;
+    }
+
+    public TMeasure Value { get; }
+
+    public static TMeasure ZeroValue => throw new NotImplementedException();
+
+    public TMeasure ToValue() => Value;
+
+    public abstract MeasureWrapper<TMeasure> Add(MeasureWrapper<TMeasure> value);
+
+    public abstract MeasureWrapper<TMeasure> Sub(MeasureWrapper<TMeasure> value);
+
+    public abstract MeasureWrapper<TMeasure> Multiply(MeasureWrapper<TMeasure> value);
+
+    public abstract MeasureWrapper<TMeasure> Divide(MeasureWrapper<TMeasure> value);
+    
+    public abstract MeasureWrapper<TMeasure> Sqrt();
+
+    public abstract bool Equals(MeasureWrapper<TMeasure>? other);
+
+    public abstract int CompareTo(object? obj);
+}
